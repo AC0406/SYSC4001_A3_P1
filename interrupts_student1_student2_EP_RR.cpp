3c339a9b90d1911f684025190eb070f4a9912dd4
root <root@ALVAN-PC.localdomain>
2025-11-30 19:44:00 -0500

    Completed assignment

--- interrupts_student1_student2_EP_RR.cpp
+++ interrupts_student1_student2_EP_RR.cpp
@@ -1,106 +1,161 @@
 /**
- * @file interrupts.cpp
- * @author Sasisekhar Govind
- * @brief template main.cpp file for Assignment 3 Part 1 of SYSC4001
- * 
- */
+* @file interrupts_EP_RR.cpp
+* @author Sasisekhar Govind
+* @brief External Priority with Preemption and Round Robin for Assignment 3 Part 1
+*/
 
-#include<interrupts_student1_student2.hpp>
+#include "interrupts_student1_student2.hpp"
 
-void FCFS(std::vector<PCB> &ready_queue) {
-    std::sort( 
-                ready_queue.begin(),
-                ready_queue.end(),
-                []( const PCB &first, const PCB &second ){
-                    return (first.arrival_time > second.arrival_time); 
-                } 
-            );
+const unsigned int TIME_QUANTUM = 100;
+
+// Priority with RR for same priority level
+void priority_rr_scheduler(std::vector<PCB> &ready_queue) {
+    std::sort(
+        ready_queue.begin(),
+        ready_queue.end(),
+        [](const PCB &first, const PCB &second){
+            if(first.priority == second.priority) {
+                return (first.arrival_time > second.arrival_time);
+            }
+            return (first.priority > second.priority);
+        }
+    );
 }
 
-std::tuple<std::string /* add std::string for bonus mark */ > run_simulation(std::vector<PCB> list_processes) {
-
-    std::vector<PCB> ready_queue;   //The ready queue of processes
-    std::vector<PCB> wait_queue;    //The wait queue of processes
-    std::vector<PCB> job_list;      //A list to keep track of all the processes. This is similar
-                                    //to the "Process, Arrival time, Burst time" table that you
-                                    //see in questions. You don't need to use it, I put it here
-                                    //to make the code easier :).
-
+std::tuple<std::string> run_simulation(std::vector<PCB> list_processes) {
+    std::vector<PCB> ready_queue;
+    std::vector<PCB> wait_queue;
+    std::vector<PCB> job_list;
     unsigned int current_time = 0;
     PCB running;
-
-    //Initialize an empty running process
     idle_CPU(running);
-
     std::string execution_status;
-
-    //make the output table (the header row)
     execution_status = print_exec_header();
-
-    //Loop while till there are no ready or waiting processes.
-    //This is the main reason I have job_list, you don't have to use it.
+    
+    unsigned int time_quantum_remaining = TIME_QUANTUM;
+    unsigned int time_since_last_io = 0;
+    
     while(!all_process_terminated(job_list) || job_list.empty()) {
-
-        //Inside this loop, there are three things you must do:
-        // 1) Populate the ready queue with processes as they arrive
-        // 2) Manage the wait queue
-        // 3) Schedule processes from the ready queue
-
-        //Population of ready queue is given to you as an example.
-        //Go through the list of proceeses
+        
+        // 1) Populate ready queue with arriving processes
         for(auto &process : list_processes) {
-            if(process.arrival_time == current_time) {//check if the AT = current time
-                //if so, assign memory and put the process into the ready queue
-                assign_memory(process);
-
-                process.state = READY;  //Set the process state to READY
-                ready_queue.push_back(process); //Add the process to the ready queue
-                job_list.push_back(process); //Add it to the list of processes
-
-                execution_status += print_exec_status(current_time, process.PID, NEW, READY);
+            if(process.arrival_time == current_time) {
+                if(assign_memory(process)) {
+                    process.state = READY;
+                    ready_queue.push_back(process);
+                    job_list.push_back(process);
+                    execution_status += print_exec_status(current_time, process.PID, NEW, READY);
+                }
             }
         }
-
-        ///////////////////////MANAGE WAIT QUEUE/////////////////////////
-        //This mainly involves keeping track of how long a process must remain in the ready queue
-
-        /////////////////////////////////////////////////////////////////
-
-        //////////////////////////SCHEDULER//////////////////////////////
-        FCFS(ready_queue); //example of FCFS is shown here
-        /////////////////////////////////////////////////////////////////
-
+        
+        // 2) Manage wait queue
+        std::vector<PCB> temp_wait;
+        for(auto &process : wait_queue) {
+            process.time_in_io++;
+            if(process.time_in_io >= process.io_duration) {
+                process.state = READY;
+                process.time_in_io = 0;
+                ready_queue.push_back(process);
+                sync_queue(job_list, process);
+                execution_status += print_exec_status(current_time, process.PID, WAITING, READY);
+            } else {
+                temp_wait.push_back(process);
+            }
+        }
+        wait_queue = temp_wait;
+        
+        // Check for preemption - if a higher priority process arrived
+        if(running.state == RUNNING && !ready_queue.empty()) {
+            priority_rr_scheduler(ready_queue);
+            if(ready_queue.back().priority < running.priority) {
+                // Preempt current process
+                states old_state = running.state;
+                running.state = READY;
+                ready_queue.push_back(running);
+                sync_queue(job_list, running);
+                execution_status += print_exec_status(current_time, running.PID, old_state, READY);
+                idle_CPU(running);
+                time_quantum_remaining = TIME_QUANTUM;
+                time_since_last_io = 0;
+            }
+        }
+        
+        // 3) Priority + Round Robin Scheduling
+        if(running.state == RUNNING) {
+            running.remaining_time--;
+            time_quantum_remaining--;
+            time_since_last_io++;
+            
+            // Check if process needs I/O
+            if(running.io_freq > 0 && time_since_last_io >= running.io_freq && running.remaining_time > 0) {
+                states old_state = running.state;
+                running.state = WAITING;
+                running.time_in_io = 0;
+                wait_queue.push_back(running);
+                sync_queue(job_list, running);
+                execution_status += print_exec_status(current_time, running.PID, old_state, WAITING);
+                idle_CPU(running);
+                time_quantum_remaining = TIME_QUANTUM;
+                time_since_last_io = 0;
+            } else if(running.remaining_time == 0) {
+                // Process completed
+                execution_status += print_exec_status(current_time, running.PID, RUNNING, TERMINATED);
+                terminate_process(running, job_list);
+                idle_CPU(running);
+                time_quantum_remaining = TIME_QUANTUM;
+                time_since_last_io = 0;
+            } else if(time_quantum_remaining == 0) {
+                // Time quantum expired
+                states old_state = running.state;
+                running.state = READY;
+                ready_queue.insert(ready_queue.begin(), running);
+                sync_queue(job_list, running);
+                execution_status += print_exec_status(current_time, running.PID, old_state, READY);
+                idle_CPU(running);
+                time_quantum_remaining = TIME_QUANTUM;
+                time_since_last_io = 0;
+            }
+        }
+        
+        // Schedule new process if CPU is idle
+        if(running.state == NOT_ASSIGNED && !ready_queue.empty()) {
+            priority_rr_scheduler(ready_queue);
+            states old_state = ready_queue.back().state;
+            run_process(running, job_list, ready_queue, current_time);
+            execution_status += print_exec_status(current_time, running.PID, old_state, RUNNING);
+            time_quantum_remaining = TIME_QUANTUM;
+            time_since_last_io = 0;
+        }
+        
+        current_time++;
+        
+        if(current_time > 100000) {
+            std::cerr << "Simulation timeout" << std::endl;
+            break;
+        }
     }
     
-    //Close the output table
     execution_status += print_exec_footer();
-
     return std::make_tuple(execution_status);
 }
 
-
 int main(int argc, char** argv) {
-
-    //Get the input file from the user
     if(argc != 2) {
         std::cout << "ERROR!\nExpected 1 argument, received " << argc - 1 << std::endl;
-        std::cout << "To run the program, do: ./interrutps <your_input_file.txt>" << std::endl;
+        std::cout << "To run the program, do: ./interrupts <input_file>" << std::endl;
         return -1;
     }
-
-    //Open the input file
+    
     auto file_name = argv[1];
     std::ifstream input_file;
     input_file.open(file_name);
-
-    //Ensure that the file actually opens
+    
     if (!input_file.is_open()) {
         std::cerr << "Error: Unable to open file: " << file_name << std::endl;
         return -1;
     }
-
-    //Parse the entire input file and populate a vector of PCBs.
-    //To do so, the add_process() helper function is used (see include file).
+    
     std::string line;
     std::vector<PCB> list_process;
     while(std::getline(input_file, line)) {
@@ -108,12 +163,11 @@ int main(int argc, char** argv) {
         auto new_process = add_process(input_tokens);
         list_process.push_back(new_process);
     }
+    
     input_file.close();
-
-    //With the list of processes, run the simulation
+    
     auto [exec] = run_simulation(list_process);
-
     write_output(exec, "execution.txt");
-
+    
     return 0;
 }
